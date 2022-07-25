c47b26c8edfc1ffc291f36dc57d8c7d111509903
jerrydumas <dumas.jerry@gmail.com>
2022-07-25 02:22:31 -0400

    3rd update

---
+++ js/data.js
@@ -0,0 +1,57 @@
+const MenuItems = [{
+        title: 'home',
+        url: '#top',
+    },
+    {
+        title: 'about',
+        url: '#contact',
+    },
+    {
+        title: 'portfolio',
+        url: '#portfolio',
+    },
+    {
+        title: 'blog',
+        url: '#blog',
+    },
+    {
+        title: 'contact',
+        url: '#contact',
+    }
+]
+const CardItems = [{
+        title: 'plan for it',
+        about: 'plan for it',
+        img: 'img/miamoessingerSN61sdC4e4Eunsplash.jpg',
+        class: 'img1',
+        link: '#'
+    },
+    {
+        title: 'shoot for the stars',
+        about: 'site to showcase products',
+        img: 'img/concept1.png',
+        class: 'img2',
+        link: '#'
+    },
+    {
+        title: 'gotham yellow',
+        about: 'nyc taxi company',
+        img: 'img/gothamyellowcar.png',
+        class: 'img3',
+        link: 'http://localhost/gothamyellow/'
+    }
+]
+const BlogItems = [{
+        title: 'Hello World',
+        desc: 'Get Excited. If you are not excited you should be.',
+        img: 'img/maik-jonietz-nZcMWOKAJrY-unsplash.jpg',
+        alt: 'img of code'
+    },
+    {
+        title: 'What\'s a Stack?',
+        desc: 'What\'s your stack?',
+        img: 'img/ben-gAe1pHGc6ms-unsplash.jpg',
+        alt: 'img of php'
+    },
+
+]
