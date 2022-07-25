5ec5bb2dc5599db40a68003e83ad6fd1d01f4e7f
jerrydumas <dumas.jerry@gmail.com>
2022-07-25 01:27:02 -0400

    Delete components directory

--- components/hero.php
+++
@@ -1,9 +0,0 @@
-<section class="hero">
-    <div class="heroContent">
-        <h1>Jerry Dumas</h1>
-        <h2>Web Designer / Developer</h2>
-        <a href="#contact">
-            <button class="btn">About Me</button>
-        </a>
-    </div>
-</section>
