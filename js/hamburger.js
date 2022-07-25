26051bae3a52f3a67531def5bf4be1ab6765155b
jerrydumas <dumas.jerry@gmail.com>
2022-07-25 02:40:29 -0400

    Image update

---
+++ js/hamburger.js
@@ -0,0 +1,6 @@
+const hamburger = document.querySelector('.hamburger');
+const links = document.querySelector('.links');
+
+hamburger.addEventListener('click', function () {
+    links.classList.toggle('open')
+})
